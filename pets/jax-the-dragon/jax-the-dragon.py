071878dbd4a66fe57178ee1cb0f43ef705e64a4c
esemv07 <143578634+esemv07@users.noreply.github.com>
2025-02-04 19:11:16 +1100

    Added Walk Anim, Sad Jax, Start Screen

--- pets/jax-the-dragon/jax-the-dragon.py
+++ pets/jax-the-dragon/jax-the-dragon.py
@@ -14,6 +14,7 @@ splash = displayio.Group(scale=scale)
 display.show(splash)
 
 outback_background = displayio.OnDiskBitmap("outback-background.bmp")
+
 bg_sprite = displayio.TileGrid(
 	outback_background,
 	pixel_shader=outback_background.pixel_shader
@@ -24,11 +25,11 @@ tile_height = 32
 
 splash.append(bg_sprite)
 
-dragon_sheet = displayio.OnDiskBitmap("jax-idle.bmp")
+dragon_sheet_idle = displayio.OnDiskBitmap("jax-idle.bmp")
 
-dragon_sprite = displayio.TileGrid(
-	dragon_sheet,
-	pixel_shader=dragon_sheet.pixel_shader,
+dragon_sprite_idle = displayio.TileGrid(
+	dragon_sheet_idle,
+	pixel_shader=dragon_sheet_idle.pixel_shader,
 	width=1,
 	height=1,
 	tile_width = tile_width,
@@ -38,7 +39,58 @@ dragon_sprite = displayio.TileGrid(
 	y=display.height - tile_height - 40
 )
 
-splash.append(dragon_sprite)
+splash.append(dragon_sprite_idle)
+
+
+dragon_sheet_walk = displayio.OnDiskBitmap("jax-walk.bmp")
+
+dragon_sprite_walk = displayio.TileGrid(
+	dragon_sheet_walk,
+	pixel_shader=dragon_sheet_walk.pixel_shader,
+	width=1,
+	height=1,
+	tile_width = tile_width,
+	tile_height = tile_height,
+	default_tile=0,
+	x=(display.width - tile_width) // 2,
+	y=display.height - tile_height - 40
+)
+
+splash.append(dragon_sprite_walk)
+
+
+dragon_sheet_idle_sad = displayio.OnDiskBitmap("jax-idle-sad.bmp")
+
+dragon_sprite_idle_sad = displayio.TileGrid(
+	dragon_sheet_idle_sad,
+	pixel_shader=dragon_sheet_idle_sad.pixel_shader,
+	width=1,
+	height=1,
+	tile_width = tile_width,
+	tile_height = tile_height,
+	default_tile=0,
+	x=(display.width - tile_width) // 2,
+	y=display.height - tile_height - 40
+)
+
+splash.append(dragon_sprite_idle_sad)
+
+
+dragon_sheet_walk_sad = displayio.OnDiskBitmap("jax-walk-sad.bmp")
+
+dragon_sprite_walk_sad = displayio.TileGrid(
+	dragon_sheet_idle_sad,
+	pixel_shader=dragon_sheet_walk_sad.pixel_shader,
+	width=1,
+	height=1,
+	tile_width = tile_width,
+	tile_height = tile_height,
+	default_tile=0,
+	x=(display.width - tile_width) // 2,
+	y=display.height - tile_height - 40
+)
+
+splash.append(dragon_sprite_walk_sad)
 
 
 waterdrop_bitmap = displayio.OnDiskBitmap("water-droplet.bmp")
@@ -116,7 +168,6 @@ splash.append(health_bar.group)
 
 death = displayio.OnDiskBitmap("restart.bmp")
 
-
 death_hi = displayio.TileGrid(
 	death,
 	pixel_shader=death.pixel_shader,
@@ -130,12 +181,30 @@ death_hi = displayio.TileGrid(
 )
 
 
+start = displayio.OnDiskBitmap("start.bmp")
+
+start_screen = displayio.TileGrid(
+	start,
+	pixel_shader=start.pixel_shader,
+	width=1,
+	height=1,
+	tile_width=128,
+	tile_height=128,
+	default_tile=0,
+	x=0,  
+	y=0
+)
+
+splash.append(start_screen)
+
 
 frame = 0
 speed = 4
 game_over = False
+game_started = False
 
 while True:
+
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -143,7 +212,7 @@ while True:
 			exit()
 
 
-	if health_bar.hp > 0:
+	if health_bar.hp > 0 and game_started == True:
 		if death_hi in splash:
 			splash.remove(death_hi)
 		health_bar.update(health_bar.hp - HEALTH_DECAY_RATE)
@@ -153,11 +222,11 @@ while True:
 			if waterdrop.y > (display.height - 30):
 				splash.remove(waterdrop)
 				waterdrops.remove(waterdrop)
-			elif check_collision(dragon_sprite, waterdrop):
+			elif check_collision(dragon_sprite_idle, waterdrop):
 				health_bar.update(health_bar.hp + 20)
 				splash.remove(waterdrop)
 				waterdrops.remove(waterdrop)
-	else:
+	elif health_bar.hp <= 0:
 		for i in waterdrops:
 			splash.remove(i)
 		waterdrops.clear()
@@ -168,11 +237,57 @@ while True:
 
 	keys = pygame.key.get_pressed()
 
-	if game_over == False:
+	if game_started == False:
+		if keys[pygame.K_RIGHT] and keys[pygame.K_LEFT]:
+			game_started = True
+			splash.remove(start_screen)
+		if keys[pygame.K_SPACE]:
+			pygame.quit()
+			exit()
+
+	if (game_over == False) and (game_started == True):
 		if keys[pygame.K_LEFT]:
-			dragon_sprite.x -= speed
+			dragon_sprite_idle.x -= speed
+			dragon_sprite_walk.x -= speed
+			dragon_sprite_idle_sad.x -= speed
+			dragon_sprite_walk_sad.x -= speed
+			if health_bar.hp > 40:
+				dragon_sprite_idle.hidden = True
+				dragon_sprite_walk.hidden = False
+				dragon_sprite_idle_sad.hidden = True
+				dragon_sprite_walk_sad.hidden = True
+			if health_bar.hp <= 40:
+				dragon_sprite_idle.hidden = True
+				dragon_sprite_walk.hidden = True
+				dragon_sprite_idle_sad.hidden = True
+				dragon_sprite_walk_sad.hidden = False
 		if keys[pygame.K_RIGHT]:
-			dragon_sprite.x += speed
+			dragon_sprite_idle.x += speed
+			dragon_sprite_walk.x += speed
+			dragon_sprite_idle_sad.x += speed
+			dragon_sprite_walk_sad.x += speed
+			if health_bar.hp > 40:
+				dragon_sprite_idle.hidden = True
+				dragon_sprite_walk.hidden = False
+				dragon_sprite_idle_sad.hidden = True
+				dragon_sprite_walk_sad.hidden = True
+			if health_bar.hp <= 40:
+				dragon_sprite_idle.hidden = True
+				dragon_sprite_walk.hidden = True
+				dragon_sprite_idle_sad.hidden = True
+				dragon_sprite_walk_sad.hidden = False
+		elif (not keys[pygame.K_LEFT]) and (not keys[pygame.K_RIGHT]):
+			if health_bar.hp > 40:
+				dragon_sprite_walk.hidden = True
+				dragon_sprite_idle.hidden = False
+				dragon_sprite_walk_sad.hidden = True
+				dragon_sprite_idle_sad.hidden = True
+			if health_bar.hp <= 40:
+				dragon_sprite_walk.hidden = True
+				dragon_sprite_idle.hidden = True
+				dragon_sprite_walk_sad.hidden = True
+				dragon_sprite_idle_sad.hidden = False
+	if (game_over == False) and (game_started == True):
 		if random.random() < 0.05:
 			spawn_waterdrop()
 		if keys[pygame.K_SPACE]:
@@ -185,11 +300,14 @@ while True:
 		if keys[pygame.K_RIGHT] and keys[pygame.K_LEFT]:
 			health_bar.hp = 100
 			game_over = False
+			splash.remove(death_hi)
 
 
-	dragon_sprite[0] = frame
-	frame = (frame + 1) % (dragon_sheet.width // tile_width)
+	dragon_sprite_walk_sad[0] = frame
+	dragon_sprite_idle_sad[0] = frame
+	dragon_sprite_walk[0] = frame
+	dragon_sprite_idle[0] = frame
+	frame = (frame + 1) % (dragon_sheet_idle.width // tile_width)
 
 	time.sleep(0.15)
-
 
--- pets/jax-the-dragon/test_script.py
+++ pets/jax-the-dragon/test_script.py
@@ -5,4 +5,7 @@ except ImportError:
     print("No hardware platform detected. Using mock board.")
 
 import displayio
-import adafruit_d
+import adafruit_displayio_pygamedisplay
+
+# This line will now run without hardware-related warnings or issues
+print("blinka-displayio-pygamedisplay is installed correctly!")
