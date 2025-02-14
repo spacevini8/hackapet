b8a0755f0f29d740399e494e4cee576e16a24105
ShibamRoy9826 <royshibam9826@gmail.com>
2025-02-14 23:28:58 +0530

    Added game over menu and a dangerous sword

--- pets/KawaiiPet/main.py
+++ pets/KawaiiPet/main.py
@@ -36,6 +36,12 @@ bg=displayio.TileGrid(
     pixel_shader=bg_img.pixel_shader,
 )
 
+gameOverImg=displayio.OnDiskBitmap("images/bg/Menu.bmp")
+gameOverBg=displayio.TileGrid(
+    gameOverImg,
+    pixel_shader=gameOverImg.pixel_shader,
+)
+
 petImg=displayio.OnDiskBitmap("images/Pets/KawaiiDog/KawaiiDog_scaled.bmp")
 pet=displayio.TileGrid(
     petImg,
@@ -51,12 +57,16 @@ pet=displayio.TileGrid(
 
 b_img=displayio.OnDiskBitmap("images/Collectibles/Bone.bmp")
 bomb_img=displayio.OnDiskBitmap("images/Collectibles/Bomb.bmp")
+sword_img=displayio.OnDiskBitmap("images/Collectibles/Sword.bmp")
 
 splash.append(menuBg)
 
 framePet=0
+bombFrame=0
+swordFrame=0
 bones=[]
 bombs=[]
+swords=[]
 
 def checkHs():
     with open("highscore","r") as f:
@@ -132,6 +142,20 @@ def spawnBomb():
     )
     bombs.append(bo)
     splash.append(bo)
+def spawnSword():
+    xPos=randint(0,screen.width-b_img.width)
+    sw=displayio.TileGrid(
+        sword_img,
+        pixel_shader=sword_img.pixel_shader,
+        width=1,
+        height=1,
+        tile_width=32,
+        tile_height=32,
+        x=xPos,
+        y=-30
+    )
+    swords.append(sw)
+    splash.append(sw)
 
 f=0
 bgFrame=0
@@ -165,23 +189,41 @@ while True:
                 bones.remove(bone)
                 scoreText.text="Score:"+str(points)
         for bomb in bombs:
+            bomb[0]=bombFrame
             bomb.y+=bone_speed
             if check_collision(pet,bomb):
-                points=0
+                points-=100
                 splash.remove(bomb)
                 bombs.remove(bomb)
                 scoreText.text="Score:"+str(points)
 
+        for sword in swords:
+            sword[0]=swordFrame
+            sword.y+=bone_speed
+            if check_collision(pet,sword):
+                points=0
+                splash.remove(sword)
+                swords.remove(sword)
+                scoreText.text="Score:"+str(points)
+                menuType=2
+                splash.append(gameOverBg)
+
         pet[0]=framePet
+        
         framePet=(framePet+1) % 10
         allFrames=(allFrames+1)%200
+        bombFrame=(framePet+1)%4
+        swordFrame=(framePet+1)%4
 
         if allFrames==1:
             spawnBomb()
             bone_speed+=2
             if bone_speed>=30:
                 bone_speed=40
-    else:
+        elif allFrames==100:
+            spawnSword()
+
+    elif menuType==0:
         if keys[pg.K_RETURN]:
             menuType=1
             splash.remove(menuBg)
@@ -191,6 +233,10 @@ while True:
             splash.append(pet)
             splash.append(scoreText)
             splash.append(hsText)
+    else:
+        if keys[pg.K_RETURN]:
+            menuType=1
+            splash.remove(gameOverBg)
     f+=1
     if f%5==0:
         bgFrame=(bgFrame+1)%12
